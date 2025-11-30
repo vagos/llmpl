d75ca6a8a237ec2f4bac8f88e92e52c9bf2509f8
Evangelos Lamprou <vagos@lamprou.xyz>
2025-11-30 02:33:43 -0500

    Add pack.pl
    Signed-off-by: Evangelos Lamprou <vagos@lamprou.xyz>

---
+++ pack.pl
@@ -0,0 +1,7 @@
+name('pllm').
+version('0.1.0').
+title('Prolog library for interfacing with large language models.').
+author('Evangelos Lamprou', 'vagos@lamprou.xyz').
+
+home('https://github.com/vagos/pllm').
+download('https://github.com/vagos/pllm/releases/*.zip').
