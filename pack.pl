1a0321e00706edc96c4cb088f619152f21d32c9f
Evangelos Lamprou <vagos@lamprou.xyz>
2025-11-30 23:42:36 -0500

    Rename
    Signed-off-by: Evangelos Lamprou <vagos@lamprou.xyz>

--- pack.pl
+++ pack.pl
@@ -1,7 +1,7 @@
-name('pllm').
+name('llmpl').
 version('0.1.0').
 title('Prolog library for interfacing with large language models.').
 author('Evangelos Lamprou', 'vagos@lamprou.xyz').
 
-home('https://github.com/vagos/pllm').
-download('https://github.com/vagos/pllm/releases/*.zip').
+home('https://github.com/vagos/llmpl').
+download('https://github.com/vagos/llmpl/releases/*.zip').
